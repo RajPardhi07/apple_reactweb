469664bf5fa455c901d952de9a7eb0bd331c2e07
Raj <bsc2rajpardhi@ifscnagpur.in>
2024-01-11 10:24:58 +0530

    page7 remove

--- src/Components/Page7.jsx
+++ src/Components/Page7.jsx
@@ -1,114 +1,114 @@
 
-const page7 = () => {
-  return (
-    <div>
-      <div className="relative w-[100%] h-[100vh] bg-gray-300  flex items-center justify-center">
+// const page7 = () => {
+//   return (
+//     <div>
+//       <div className="relative w-[100%] h-[100vh] bg-gray-300  flex items-center justify-center">
 
-        <div className="w-[70%] h-[95%]  mt-3">
-          <div className="w-[100%] h-[40%] text-black-400">
+//         <div className="w-[70%] h-[95%]  mt-3">
+//           <div className="w-[100%] h-[40%] text-black-400">
 
-            <p className="text-[13px] mb-4">*Instant savings, otherwise referred to as instant cashback, is available with the purchase of an eligible product for qualifying HDFC Bank Credit Cards and EasyEMI Credit Cards only. Minimum transaction value of ₹10001.00 applies. Click here to see instant savings amounts and eligible devices. Instant savings is available for up to two orders per rolling 90-day period with an eligible card. Card eligibility is subject to terms and conditions between you and your card-issuing bank. Total transaction value is calculated after any trade-in credit or eligible discount applied. Any subsequent order adjustment(s) or cancellation(s) may result in instant savings being recalculated, and any refund may be adjusted to account for instant savings clawback; this may result in no refund being made to you. Offer may be revised or withdrawn at any time without any prior notice. Additional terms apply. Offer cannot be combined with Apple Store for Education or Corporate Employee Purchase Plan pricing. Multiple separate orders cannot be combined for instant savings.
-            </p>
+//             <p className="text-[13px] mb-4">*Instant savings, otherwise referred to as instant cashback, is available with the purchase of an eligible product for qualifying HDFC Bank Credit Cards and EasyEMI Credit Cards only. Minimum transaction value of ₹10001.00 applies. Click here to see instant savings amounts and eligible devices. Instant savings is available for up to two orders per rolling 90-day period with an eligible card. Card eligibility is subject to terms and conditions between you and your card-issuing bank. Total transaction value is calculated after any trade-in credit or eligible discount applied. Any subsequent order adjustment(s) or cancellation(s) may result in instant savings being recalculated, and any refund may be adjusted to account for instant savings clawback; this may result in no refund being made to you. Offer may be revised or withdrawn at any time without any prior notice. Additional terms apply. Offer cannot be combined with Apple Store for Education or Corporate Employee Purchase Plan pricing. Multiple separate orders cannot be combined for instant savings.
+//             </p>
 
-            <h6 className="text-[13px] mt-2">
-              ‡No Cost EMI is available with the purchase of an eligible product made using eligible cards on 3- or 6-month tenures from most leading banks. Monthly pricing is rounded to the nearest rupee. Exact pricing will be provided by your bank, subject to your bank’s terms and conditions. Minimum order spend applies as per your card-issuing bank threshold. Offer cannot be combined with Apple Store for Education or Corporate Employee Purchase Plan pricing. Card eligibility is subject to terms and conditions between you and your card-issuing bank. Offer may be revised or withdrawn at any time without any prior notice. Additional terms apply.
-              Representative example: Based on a 6-month tenure. ₹79900.00 total cost includes 15% p.a. and No Cost EMI savings of ₹3380.00, paid over 6 months as six monthly payments of ₹13317.00.
-              A subscription is required for Apple TV+.
-            </h6>
-          </div>
-          <div className="w-[100%] h-[60%] text-black">
+//             <h6 className="text-[13px] mt-2">
+//               ‡No Cost EMI is available with the purchase of an eligible product made using eligible cards on 3- or 6-month tenures from most leading banks. Monthly pricing is rounded to the nearest rupee. Exact pricing will be provided by your bank, subject to your bank’s terms and conditions. Minimum order spend applies as per your card-issuing bank threshold. Offer cannot be combined with Apple Store for Education or Corporate Employee Purchase Plan pricing. Card eligibility is subject to terms and conditions between you and your card-issuing bank. Offer may be revised or withdrawn at any time without any prior notice. Additional terms apply.
+//               Representative example: Based on a 6-month tenure. ₹79900.00 total cost includes 15% p.a. and No Cost EMI savings of ₹3380.00, paid over 6 months as six monthly payments of ₹13317.00.
+//               A subscription is required for Apple TV+.
+//             </h6>
+//           </div>
+//           <div className="w-[100%] h-[60%] text-black">
 
-            <div className="flex pl-[2vw]  justify-between">
-
-
-              <div className="w-[6vw] h-[45vh] font-light	">
-                Shop and Learn
-                Store
-                Mac
-                iPad
-                iPhone
-                Watch
-                AirPods
-                TV & Home
-                AirTag
-                Accessories
-                Gift Cards
-                Apple Wallet
-                Wallet
-              </div>
+//             <div className="flex pl-[2vw]  justify-between">
 
 
-              <div className="w-[6vw] text-[18px] font-light">
-                Account
-                Manage Your Apple ID
-                Apple Store Account
-                iCloud.com
-                Entertainment
-                Apple One
-                Apple TV+
-                Apple Music
-                Apple Arcade
-                Apple Podcasts
-                Apple Books
-                App Store
-              </div>
-
-              <div className="w-[6vw] text-[18px] font-light">
-                Account
-                Manage Your Apple ID
-                Apple Store Account
-                iCloud.com
-                Entertainment
-                Apple One
-                Apple TV+
-                Apple Music
-                Apple Arcade
-                Apple Podcasts
-                Apple Books
-                App Store
-              </div>
-
-              <div className="w-[6vw] text-[18px] font-light">
-                Account
-                Manage Your Apple ID
-                Apple Store Account
-                iCloud.com
-                Entertainment
-                Apple One
-                Apple TV+
-                Apple Music
-                Apple Arcade
-                Apple Podcasts
-                Apple Books
-                App Store
-              </div>
-
-              <div className="w-[6vw] text-[18px] font-light">
-                Account
-                Manage Your Apple ID
-                Apple Store Account
-                iCloud.com
-                Entertainment
-                Apple One
-                Apple TV+
-                Apple Music
-                Apple Arcade
-                Apple Podcasts
-                Apple Books
-                App Store
-              </div>
-
-            </div>
-          </div>
+//               <div className="w-[6vw] h-[45vh] font-light	">
+//                 Shop and Learn
+//                 Store
+//                 Mac
+//                 iPad
+//                 iPhone
+//                 Watch
+//                 AirPods
+//                 TV & Home
+//                 AirTag
+//                 Accessories
+//                 Gift Cards
+//                 Apple Wallet
+//                 Wallet
+//               </div>
 
 
+//               <div className="w-[6vw] text-[18px] font-light">
+//                 Account
+//                 Manage Your Apple ID
+//                 Apple Store Account
+//                 iCloud.com
+//                 Entertainment
+//                 Apple One
+//                 Apple TV+
+//                 Apple Music
+//                 Apple Arcade
+//                 Apple Podcasts
+//                 Apple Books
+//                 App Store
+//               </div>
+
+//               <div className="w-[6vw] text-[18px] font-light">
+//                 Account
+//                 Manage Your Apple ID
+//                 Apple Store Account
+//                 iCloud.com
+//                 Entertainment
+//                 Apple One
+//                 Apple TV+
+//                 Apple Music
+//                 Apple Arcade
+//                 Apple Podcasts
+//                 Apple Books
+//                 App Store
+//               </div>
+
+//               <div className="w-[6vw] text-[18px] font-light">
+//                 Account
+//                 Manage Your Apple ID
+//                 Apple Store Account
+//                 iCloud.com
+//                 Entertainment
+//                 Apple One
+//                 Apple TV+
+//                 Apple Music
+//                 Apple Arcade
+//                 Apple Podcasts
+//                 Apple Books
+//                 App Store
+//               </div>
+
+//               <div className="w-[6vw] text-[18px] font-light">
+//                 Account
+//                 Manage Your Apple ID
+//                 Apple Store Account
+//                 iCloud.com
+//                 Entertainment
+//                 Apple One
+//                 Apple TV+
+//                 Apple Music
+//                 Apple Arcade
+//                 Apple Podcasts
+//                 Apple Books
+//                 App Store
+//               </div>
+
+//             </div>
+//           </div>
 
 
 
-        </div>
-      </div>
-    </div >
-  )
-}
 
-export default page7
+
+//         </div>
+//       </div>
+//     </div >
+//   )
+// }
+
+// export default page7
